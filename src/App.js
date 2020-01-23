d58163b3bed5800aca5e059cff3c659fc3d861a5
edwardwang0302 <edwardwang0302@me.com>
2020-01-23 10:43:40 +0800

    Recommend & singer page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-
+import React from 'react'
+import { Provider } from 'react-redux'
+import { GlobalStyle } from  './style'
+import { renderRoutes } from 'react-router-config'
 import { IconStyle } from './assets/iconfont/iconfont'
-import { GlobalStyle } from './style'
+import store from './store/index'
+import routes from './routes/index.js'
+import { HashRouter } from 'react-router-dom';
+import { Data } from './application/Singers/data';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
-  );
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        <Data>
+          { renderRoutes(routes) }
+        </Data>
+      </HashRouter>
+    </Provider>
+  )
 }
 
 export default App;
